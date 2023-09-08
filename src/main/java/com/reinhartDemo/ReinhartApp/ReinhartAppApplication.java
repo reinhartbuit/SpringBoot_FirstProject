57ea7554d07af8f5c00e31a2bda89b530c2df130
Reinhart <buit.reinhart@gmail.com>
2023-09-08 14:15:54 +0200

    Initial commit

---
+++ src/main/java/com/reinhartDemo/ReinhartApp/ReinhartAppApplication.java
@@ -0,0 +1,13 @@
+package com.reinhartDemo.ReinhartApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinhartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinhartAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reinhartDemo/ReinhartApp/ReinhartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinhartDemo.ReinhartApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinhartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
